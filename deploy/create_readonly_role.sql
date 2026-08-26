e73f940dfd7b857ad03898845a6cb07d797cebe4
kingboy20230509 <132969194+kingboy20230509@users.noreply.github.com>
2026-08-26 21:52:14 +0800

    fix: remove shell output from usage SQL

--- deploy/create_readonly_role.sql
+++ deploy/create_readonly_role.sql
@@ -1,4 +1,3 @@
-/opt/homebrew/Library/Homebrew/cmd/shellenv.sh: line 27: /bin/ps: Operation not permitted
 \set ON_ERROR_STOP on
 
 -- Run this file as the Sub2API database owner. It creates a login that can
